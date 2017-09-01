2b9e224271e0a3b0b0955504fda0544286dbeb61
Damien Sawyer <damiensawyer@gmail.com>
2017-09-02 06:02:29 +1000

    Created Core Library

---
+++ src/CoreTestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using DbUp;
+
+namespace CoreTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cstring = "Host=127.0.0.1;Port=5432;Database=fromcore2;User Id=postgres;Password=postgres;";
+            EnsureDatabase.For.PostgresqlDatabase(cstring);
+
+            var upgrader = DeployChanges.To
+                .PostgresqlDatabase(cstring)
+                .WithScript("d233", $@"
+	
+	            CREATE TABLE contact
+            (
+	            id uuid primary key not null,
+	            date date not null
+            );
+	
+	            CREATE TABLE cycle
+            (
+              id uuid not null,
+              data json not null,
+              CONSTRAINT contactfk FOREIGN KEY(id)
+
+	              REFERENCES contact(id) MATCH SIMPLE
+
+	              ON UPDATE CASCADE ON DELETE NO ACTION
+            );
+	            ")
+                .LogToConsole()
+                .Build();
+
+            upgrader.PerformUpgrade();
+        }
+    }
+}
